e3f22067e02e49596dde61ac9422add16818e5bd
sanju <sanju@pc>
2013-11-13 01:35:12 -0500

    latest

---
+++ endsem1123/src/Story.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Story {
+
+	int priority;
+	int velocity;
+	int storyPoints;
+	List<String> task = new ArrayList<String>();
+
+}
